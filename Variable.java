f46ecdf1bc1a8007a46b7d3e7cb0c20d4c39f749
naresh123456-cpu <73584111+naresh123456-cpu@users.noreply.github.com>
2020-10-29 18:32:49 +0530

    Add files via upload

--- Variable.java
+++ Variable.java
@@ -1,34 +1,43 @@
+package Motivity;
+
+import java.util.Scanner;
+
 class Student{
-  int id;
-  float percentage;
-  static String clg;
-}
- class Employee{
-   int employee_id;
-   int salary;
-   static String company_name;
-}
-class Variable{
- public static void main(String args[])
-{
-  Student naresh=new Student();
-   naresh.id=100;
-   naresh.percentage=89.5f;
-   Student.clg= "SPEC";
-   System.out.println("==============student details==============");
-   System.out.println("student id="+naresh.id);
-   System.out.println("percentage="+naresh.percentage);
-   System.out.println("college="+Student.clg);
+	  int id;
+	  float percentage;
+	  static String clg;
+	}
+	 class Employee{
+	   int employee_id;
+	   int salary;
+	   static String company_name;
+	}
+	class Variable{
+	 public static void main(String args[])
+	{
+	  Student naresh=new Student();
+	  Scanner sc=new Scanner(System.in);
+	  System.out.println("entering the student details");
+	   naresh.id=sc.nextInt();
+	   naresh.percentage=sc.nextFloat();
+	   Student.clg= sc.next();
+	   System.out.println("==============student details==============");
+	   System.out.println("student id="+naresh.id);
+	   System.out.println("percentage="+naresh.percentage);
+	   System.out.println("college="+Student.clg);
 
 
-   Employee ramesh=new Employee();
-   ramesh.employee_id=102;
-   ramesh.salary=25000;
-   Employee.company_name="jnit";
-   System.out.println("===========employee details===============");
-   System.out.println(ramesh.employee_id);
-   System.out.println(ramesh.salary);
-   System.out.println(Employee.company_name);
-}
-}
+	   Employee ramesh=new Employee();
+	   Scanner sc1=new Scanner(System.in);
+		  System.out.println("entering the employee details");
+	   ramesh.employee_id=sc1.nextInt();
+	   ramesh.salary=sc1.nextInt();
+	   Employee.company_name=sc1.next();
+	   System.out.println("===========employee details===============");
+	   System.out.println(ramesh.employee_id);
+	   System.out.println(ramesh.salary);
+	   System.out.println(Employee.company_name);
+	}
+	}
+
 
